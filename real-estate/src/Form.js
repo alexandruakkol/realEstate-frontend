d8cb0d9721371258fa27f792dadd130c457d29e6
Alexandru <alexandru.akkol@gmail.com>
2022-03-04 13:07:23 +0200

    all form render done

--- real-estate/src/Form.js
+++ real-estate/src/Form.js
@@ -3,25 +3,28 @@ import Checks from './Checks'
 import Radios from './Radios'
 const Form = () => {
 
+    const [price,setPrice] = React.useState('');
+    const [metro,setMetro] = React.useState('');
+    const [area,setArea] = React.useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('hello');
+        console.log(price, area, metro);
     }
 
   return (
-      
     <form onSubmit={handleSubmit}>
-        <label htmlFor='price'>Asking Price</label>
-        <input type='text' id='price'></input>
+        <label htmlFor='price'>Price</label>
+        <input type='text' id='price' value={price} onChange={(e) => {setPrice(e.target.value)}}></input>
 
         <div className="areaForm">
         <label htmlFor='area'>Area (m²)</label>
-        <input type='text' id='area'></input>
+        <input type='text' id='area' value={area} onChange={(e) => {setArea(e.target.value)}}></input>
         </div>
 
         <div className="metroDiv">
         <label htmlFor='metroDistance'>Distance to metro (m)</label>
-        <input type='text' id='metroDistance'></input>
+        <input type='text' id='metroDistance' value={metro} onChange={(e) => {setMetro(e.target.value)}}></input>
         </div>
 
         <Checks>
--- real-estate/src/Radios.js
+++ real-estate/src/Radios.js
@@ -1,31 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const Radios = () => {
   return (
-    <div className='yearRadios'>
+    <section className="radios">
+      {/* Year built */}
+      <div className="yearRadios">
         <p>Building year:</p>
         <div>
-        <input type='radio' id='<1950' name='year'></input>
-        <label htmlFor='<1950'>&#60;1950</label>
+          <input type="radio" id="<1950" name="year"></input>
+          <label htmlFor="<1950">&#60;1950</label>
         </div>
 
         <div>
-        <input type='radio' id='1950-1977' name='year'></input>
-        <label htmlFor='1950-1977'>1950-1977</label>
+          <input type="radio" id="1950-1977" name="year"></input>
+          <label htmlFor="1950-1977">1950-1977</label>
         </div>
 
         <div>
-        <input type='radio' id='1977-1990' name='year'></input>
-        <label htmlFor='1977-1990'>1977-1990</label>
+          <input type="radio" id="1977-1990" name="year"></input>
+          <label htmlFor="1977-1990">1977-1990</label>
         </div>
 
         <div>
-        <input type='radio' id='>1990' name='year'></input>
-        <label htmlFor='>1990'>&#62;1990</label>
+          <input type="radio" id=">1990" name="year"></input>
+          <label htmlFor=">1990">&#62;1990</label>
+        </div>
+      </div>
+
+      {/* Floor */}
+      <div className="floorRadios">
+        <p>Floor:</p>
+        <div>
+          <input type="radio" id='groundFloor' name="floor"></input>
+          <label htmlFor="groundFloor">Ground Floor</label>
         </div>
 
-    </div>
-  )
-}
+        <div>
+          <input type="radio" id="lastFloor" name="floor"></input>
+          <label htmlFor="lastFloor">Last floor</label>
+        </div>
+      </div>
 
-export default Radios
+      {/* Condition */}
+      <div className="conditionRadios">
+        <p>Condition</p>
+        <div>
+          <input type="radio" id='shell' name="condition"></input>
+          <label htmlFor="shell">Shell stage</label>
+        </div>
+
+        <div>
+          <input type="radio" id="finish" name="condition"></input>
+          <label htmlFor="finish">Finish stage</label>
+        </div>
+
+        <div>
+          <input type="radio" id="turnkey" name="condition"></input>
+          <label htmlFor="turnkey">Turnkey</label>
+        </div>
+      </div>
+
+    </section>
+  );
+};
+
+export default Radios;
