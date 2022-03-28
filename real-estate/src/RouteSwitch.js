539049cd9f84f4280d81478784f965a0db307d33
Alexandru <alexandru.akkol@gmail.com>
2022-03-28 18:19:46 +0300

    added homepage link to logos

--- real-estate/src/RouteSwitch.js
+++ real-estate/src/RouteSwitch.js
@@ -1,9 +1,15 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import App from './App';
+import Login from './Login';
 
 const RouteSwitch = () =>{
     return(
-        <App></App>
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App></App>} />
+        <Route path="/login" element={<Login></Login>} />
+      </Routes>
+    </BrowserRouter>
     )
 }
 export default RouteSwitch;
